be12264351f046e461c7d7e3e199f1ecce9b7ba7
23thibja <59451408+23thibja@users.noreply.github.com>
2020-01-22 13:38:38 -0500

    Add files via upload

---
+++ thibeault_unit1_practice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# PART A
+print("welocme to information Technolgy\n\nProgramming and\nWeb Development")
+print("")
+print(" _       ___________\n| |     / /_ __/    |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|\n")
+
+
+
+
+# PART B
+print ("A lighting flash:\nbetween the forest trees\nI have seen water.\n\t- shuki\n")
+print("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********\n")
+print("\"Computer Science is no more about computers\nthan astronomy is abouot telescoped\"\n- Edsger W. Dijkstra\n")
+print("            )\n           (\n       ^    )\n     /  \ ___\n    /    \| |\n   /  _   \ |\n  /  (_)   \|\n /          \ \n/|  _   _    \ \n | | | | |   |\  \n | |_| |_|   |\n |  __   __  | \n | |  | |  | |      #\n | | @| |__| |     ###\n | |  |      |    ##### \n\/\/\/\/\/\/\/\/\/\/\/\/\n ")
+  
