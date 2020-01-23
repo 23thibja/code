0ef10124ee1976bf44b2f888ecfa2817d8b2f969
23thibja <59451408+23thibja@users.noreply.github.com>
2020-01-23 13:37:19 -0500

    Add files via upload

--- thibeault_unit1_practice.py
+++ thibeault_unit1_practice.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+
+
 # PART A
 print("welocme to information Technolgy\n\nProgramming and\nWeb Development")
 print("")
@@ -13,4 +16,33 @@ print ("A lighting flash:\nbetween the forest trees\nI have seen water.\n\t- shu
 print("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********\n")
 print("\"Computer Science is no more about computers\nthan astronomy is abouot telescoped\"\n- Edsger W. Dijkstra\n")
 print("            )\n           (\n       ^    )\n     /  \ ___\n    /    \| |\n   /  _   \ |\n  /  (_)   \|\n /          \ \n/|  _   _    \ \n | | | | |   |\  \n | |_| |_|   |\n |  __   __  | \n | |  | |  | |      #\n | | @| |__| |     ###\n | |  |      |    ##### \n\/\/\/\/\/\/\/\/\/\/\/\/\n ")
-  
+
+
+
+
+
+
+# PART  C
+user_name = input("What is your name?")
+favorite_animal = input("What is your favorite animal?")
+print("Hellow", user_name, ". Your favorite animal is the", favorite_animal, ".")
+
+
+number = input("What is your favorite number?")
+print(number * 3)
+
+
+num_1 = int(input("Enter a test score. "))
+num_2 = int(input("Enter another test score. "))
+num_3 = int(input("Enter one more test score. "))
+sum_results = str((num_1 + num_2 + num_3)/3)
+print(sum_results)
+
+
+ani_1 = input("Enter a animal ")
+ani_2 = input("Enter another animal ")
+ani_3 = input("Enter one more ")
+print ()
+
+
+
