0d7774ccd31bcad3f4327f127358c0abe6e6c650
23thibja <59451408+23thibja@users.noreply.github.com>
2020-01-24 08:14:23 -0500

    Add files via upload

--- thibeault_unit1_practice.py
+++ thibeault_unit1_practice.py
@@ -23,9 +23,9 @@ print("            )\n           (\n       ^    )\n     /  \ ___\n    /    \| |\
 
 
 # PART  C
-user_name = input("What is your name?")
-favorite_animal = input("What is your favorite animal?")
-print("Hellow", user_name, ". Your favorite animal is the", favorite_animal, ".")
+user_name = input("What is your name? ")
+favorite_animal = input("What is your favorite animal? ")
+print("Hellow", user_name, ". Your favorite animal is the ", favorite_animal, ".")
 
 
 number = input("What is your favorite number?")
@@ -39,10 +39,49 @@ sum_results = str((num_1 + num_2 + num_3)/3)
 print(sum_results)
 
 
-ani_1 = input("Enter a animal ")
-ani_2 = input("Enter another animal ")
-ani_3 = input("Enter one more ")
-print ()
+ani_1 = input("Enter a animal. ")
+ani_2 = input("Enter another animal. ")
+ani_3 = input("Enter one more. ")
+print (ani_3, ani_2, ani_1)
 
 
+int_1 = int(input("Enter an integer. "))
+int_2 = int(input("Enter another integer. "))
+print("  ", int_1 ,"   \n ______\n|      |\n|      |", int_2, "\n|______|\n")
+print("Area = ",int_1 * int_2)
+
+
+
+
+
+
+#Part D
+F_N = input("Please Print First Name: ")
+L_N = input("Please Print Last Name: ")
+print( L_N, ",", F_N)
+
+
+number_1 = int(input("Enter a number: "))
+print(number_1 * 8)
+
+
+number_2 = int(input("Enter a Number: "))
+print(number_2 * 12)
+
+
+
+
+
+#Part E
+
+a = input("Enter an animal: ")
+s = input("Enter a sound: ")
+e = "E-I-E-I-O"
+
+print ("Old Macdonald had a farm, " + e)
+print ("And on his farm he had a " + a + ", " + e)
+print ("With a " + s + "-" + s + " here and a " + s + "-" + s + " there")
+print ("Here a "+ s+ " there a " +s)
+print ("Everywhere a "  + s + "-" +s)
+print ("Old Macdonald had a farm," + e)
 
