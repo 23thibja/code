fda24aab8f2a98732adc9395453b219cc8c2eef2
23thibja <59451408+23thibja@users.noreply.github.com>
2020-01-24 09:42:03 -0500

    Add files via upload

---
+++ Thibeault_madlib.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+#Mad lib 1
+
+print("Let's play Mad Libs!")
+
+name = input("Enter a name: ")
+ad_1 = input("Enter a adjective: ")
+ad_2 = input("Enter another adjective: ")
+adv = input("Enter an adverb: ")
+Food_1 = input("Enter a food: ")
+Food_2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+
+print(name, "was planning a dream vacation to", place)
+print(name, "was especially looking forward to trying the local")
+print("cuisine, including", ad_1, Food_1,"and", Food_2)
+
+print(name, "will have to practice the language", adv, "to")
+print("make it easier to" , verb ,"people")
+
+print(name,"has a long list of sights to see, including the")
+print(noun, "museum and the ", ad_2, "park")
+
+#MADLIB 2
+print("\n\n\nMad Lib time!")
+
+adj_1 = input("\nEnter a adjective: ")
+adj_2 = input("Enter another adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter a room in a house: ")
+verb_t = input("Enter a verb(past tence): ")
+verb_2 = input("Enter a verb: ")
+verb_ing = input("Enter a verb ending in ing: ")
+liq = input("Enter a liquid: ")
+parts = input("Enter a body part (plural):")
+
+
+print("Mad Lib time!")
+print("\nIt was a ", adj_1,"cold November day.")
+print("I woke up to the ", adj_2 , "smell of ", bird ,"roasting in the ", room ,)
+print("downstairs")
+print("I", verb_t ,"dow the stairs to see if i could help", verb_2  ,"the dinner")
+print("So i carried a tray of", liq ,"into the", verb_ing ,"room")
+print("When i got there, i could not believe my", parts ,)
