c5394ed05abfe29ec657c30cda0043d880eebe82
23thibja <59451408+23thibja@users.noreply.github.com>
2020-03-09 13:47:02 -0400

    Add files via upload

---
+++ school-scedule.py
@@ -0,0 +1,30 @@
+First = input ("What is your first name: ")
+Last = input ("What is your last name: ")
+ID = input ("What is your student ID: ")
+class1 = input ("Enter your first class, enter Stop to end:")
+room1 = input ("Enter the room number: ")
+class2 = input ("Enter your your next class, enter Stop to end: ")
+room2 = input ("Enter the room number: ")
+class3 = input ("Enter your your next class, enter Stop to end: ")
+room3 = input ("Enter the room number: ")
+class4 = input ("Enter your your next class, enter Stop to end: ")
+room4 = input ("Enter the room number: ")
+class5 = input ("Enter your your next class, enter Stop to end: ")
+room5 = input ("Enter the room number: ")
+class6 = input ("Enter your your next class, enter Stop to end: ")
+room6 = input ("Enter the room number: ")
+
+classes = (room1, room2, room3, room4, room5, room6)
+while classes == "Stop":
+    break
+
+
+
+
+
+
+#print ("*************************************************")
+#print ("*                                               *")
+#print ("*       ",Last,"," ,First,"   ID: ",ID,"        *")
+#print ("*                                               *")
+#print ("*************************************************")
